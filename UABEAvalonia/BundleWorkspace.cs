3a8d828c9713960da217ef58305077be8ff72f26
nesquack <karsen.beck@gmail.com>
2022-11-07 12:19:36 -0600

    add better bundle support (rename/modified *, etc)

---
+++ UABEAvalonia/BundleWorkspace.cs
@@ -0,0 +1,191 @@
+﻿using AssetsTools.NET;
+using AssetsTools.NET.Extra;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UABEAvalonia
+{
+    public class BundleWorkspace
+    {
+        public BundleFileInstance? BundleInst { get; set; }
+        public AssetsManager am { get; }
+        
+        public ObservableCollection<BundleWorkspaceItem> Files { get; }
+        public Dictionary<string, BundleWorkspaceItem> FileLookup { get; }
+        public HashSet<string> RemovedFiles { get; }
+
+        public BundleWorkspace()
+        {
+            BundleInst = null;
+            am = new AssetsManager();
+
+            Files = new ObservableCollection<BundleWorkspaceItem>();
+            FileLookup = new Dictionary<string, BundleWorkspaceItem>();
+            RemovedFiles = new HashSet<string>();
+        }
+
+        public void Reset(BundleFileInstance? bundleInst)
+        {
+            BundleInst = bundleInst;
+
+            Files.Clear();
+            FileLookup.Clear();
+            RemovedFiles.Clear();
+
+            if (bundleInst != null)
+                PopulateFilesList();
+        }
+
+        private void PopulateFilesList()
+        {
+            var dirInfs = BundleInst.file.BlockAndDirInfo.DirectoryInfos;
+            foreach (var dirInf in dirInfs)
+            {
+                string name = dirInf.Name;
+                long startAddress = dirInf.Offset;
+                long length = dirInf.DecompressedSize;
+                SegmentStream stream = new SegmentStream(BundleInst.file.DataReader.BaseStream, startAddress, length);
+                BundleWorkspaceItem wsItem = new BundleWorkspaceItem(name, name, false, (dirInf.Flags & 0x04) != 0, false, stream);
+                Files.Add(wsItem);
+                FileLookup[name] = wsItem;
+            }
+        }
+
+        public void AddOrReplaceFile(Stream stream, string name, bool isSerialized, string? prevName = null)
+        {
+            if (prevName == null)
+                prevName = name;
+
+            if (FileLookup.ContainsKey(prevName))
+            {
+                BundleWorkspaceItem wsItem;
+
+                int fileListIndex = Files.IndexOf(FileLookup[prevName]);
+                if (fileListIndex != -1)
+                {
+                    wsItem = new BundleWorkspaceItem(name, Files[fileListIndex].OriginalName, false, isSerialized, true, stream);
+                    Files[fileListIndex] = wsItem;
+                }
+                else
+                {
+                    // shouldn't happen
+                    wsItem = new BundleWorkspaceItem(name, prevName, false, isSerialized, true, stream);
+                }
+
+                // don't close if not new because we would close the
+                // underlying bundle stream
+                if (FileLookup[prevName].IsNew)
+                {
+                    FileLookup[prevName].Stream.Close();
+                }
+
+                FileLookup.Remove(prevName);
+                FileLookup[name] = wsItem;
+            }
+            else
+            {
+                BundleWorkspaceItem wsItem = new BundleWorkspaceItem(name, name, false, isSerialized, true, stream);
+
+                Files.Add(wsItem);
+                FileLookup[name] = wsItem;
+            }
+        }
+
+        public void RenameFile(string origName, string newName)
+        {
+            if (FileLookup.ContainsKey(origName))
+            {
+                BundleWorkspaceItem item = FileLookup[origName];
+                item.Name = newName;
+                FileLookup.Remove(origName);
+                FileLookup[newName] = item;
+            }
+        }
+
+        public List<BundleReplacer> GetReplacers()
+        {
+            List<BundleReplacer> replacers = new List<BundleReplacer>();
+            
+            foreach (string name in RemovedFiles)
+            {
+                // hasSerializedData is ignored
+                BundleReplacer replacer = new BundleRemover(name, false);
+                replacers.Add(replacer);
+            }
+
+            foreach (BundleWorkspaceItem item in FileLookup.Values)
+            {
+                if (!item.IsRemoved)
+                {
+                    if (item.IsModified)
+                    {
+                        replacers.Add(new BundleReplacerFromStream(item.OriginalName, item.Name, item.IsSerialized, item.Stream, 0, -1));
+                    }
+                    else if (item.Name != item.OriginalName)
+                    {
+                        // hasSerializedData is ignored
+                        replacers.Add(new BundleRenamer(item.OriginalName, item.Name, false));
+                    }
+                }
+            }
+
+            return replacers;
+        }
+    }
+    
+    public class BundleWorkspaceItem
+    {
+        public string Name { get; set; }
+        public string OriginalName { get; }
+        public bool IsNew { get; }
+        public bool IsSerialized { get; }
+        public bool IsRemoved { get; set; }
+        // the difference between IsNew and IsModified is
+        // IsNew is only if it was a newly imported file
+        // but IsModified comes from edits by the info window
+        public bool IsModified { get; }
+        public Stream Stream { get; }
+        //public BundleReplacer? Replacer { get; }
+
+        public BundleWorkspaceItem(
+            string name, string originalName, bool isNew,
+            bool isSerialized, bool isModified, Stream stream
+        )
+        {
+            Name = name;
+            OriginalName = originalName;
+            IsNew = isNew;
+            IsSerialized = isSerialized;
+            IsModified = isModified;
+            Stream = stream;
+            //Replacer = null;
+
+            IsRemoved = false;
+        }
+
+        //public BundleWorkspaceItem(
+        //    string name, string originalName, bool isNew,
+        //    bool isSerialized, Stream stream, BundleReplacer replacer
+        //)
+        //{
+        //    Name = name;
+        //    OriginalName = originalName;
+        //    IsNew = isNew;
+        //    IsSerialized = isSerialized;
+        //    Stream = stream;
+        //    Replacer = replacer;
+        //
+        //    IsRemoved = false;
+        //}
+
+        public override string ToString()
+        {
+            return Name + (IsModified ? "*" : "");
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,50 +1,22 @@
 <p align="center"><img src="UABEAvalonia/Assets/logo.png" /></p>
 
-## [UABE has been updated! Go use that instead!](https://github.com/DerPopo/UABE)
-
 [Latest Nightly Build](https://nightly.link/nesrak1/UABEA/workflows/dotnet-desktop/master/uabea-windows.zip) | [Latest Release](https://github.com/nesrak1/UABEA/releases)
 
 [![GitHub issues](https://img.shields.io/github/issues/nesrak1/UABEA?logo=GitHub&style=flat-square)](https://github.com/nesrak1/UABEA/issues) [![discord](https://img.shields.io/discord/862035581491478558?label=discord&logo=discord&logoColor=FFFFFF&style=flat-square)](https://discord.gg/hd9VdswwZs)
 
-## Why UABEAvalonia
+## UABEAvalonia
 
-[UABE](https://github.com/DerPopo/UABE) had not been updated in a while, and at the time, had not been open source. So UABEA was designed as a replacement while UABE did not support newer versions. Now that UABE is open source and updated, UABEA development will most likely be stopped as it can do everything UABEA can do and more with less bugs.
+Cross-platform Asset Bundle/Serialized File reader and writer. Originally based on [UABE](github.com/SeriousCache/UABE).
 
-## New features
 
-There's not much in the way of new features as of yet (I wasn't even planning on doing this). There have been many requested features such as sprite importing/exporting, better command line support, batch import/export from gui, etc. I do not have a lot of time, but I do hope to get to these someday.
 
-## Exporting assets
+## Extracting assets
 
-UABEA can export textures and asset dumps, but that's about it. If you're trying to dump anything else, try [AssetStudio](https://github.com/Perfare/AssetStudio) or [AssetRipper](https://github.com/ds5678/AssetRipper) (uTinyRipper), but these tools cannot import again. 
+I develop UABEA as more of a modding/research tool than an extracting tool. Use [AssetRipper](https://github.com/AssetRipper/AssetRipper) or [AssetStudio](https://github.com/Perfare/AssetStudio/) if you only want to extract assets.
 
-## Scripting
+## Addressables
 
-If you're doing something that requires scripting such as dumping all of the fields from a MonoBehaviour, importing multiple text files or textures, etc. without interacting with the gui, try using [AssetsTools.NET](https://github.com/nesrak1/AssetsTools.NET) instead. UABEA can be a good way to figure out how the file is laid out, but the script can be written with AssetsTools. If AssetsTools is too complicated, you can also try [UnityPy](https://github.com/K0lb3/UnityPy) which has a simpler api with the cost of supporting less assets.
-
-## MonoBehaviours
-
-Many newer Unity games (especially non-pc games) are compiled with il2cpp which means that out of the box, UABEA cannot correctly deserialize any MonoBehaviour scripts. This is especially obvious when you export dump and import dump and find the file size much smaller. To fix this, dump il2cpp dummy dlls using a tool like [il2cppdumper](https://github.com/Perfare/Il2CppDumper) or [cpp2il](https://github.com/SamboyCoding/Cpp2IL). Then, create a folder called Managed in the same directory as the assets file/bundle file you want to open and copy all the dummy dlls generated with the tool you used into that folder.
-
-## Differences between UABE and UABEA
-
-|                                         | UABE             | UABEA                                 |
-| --------------------------------------- | ---------------- | ------------------------------------- |
-| Supported versions                      | Unity 3.4-2021.3 | Unity 5-2021.2                        |
-| Class data editor                       | Yes              | No                                    |
-| Standalone .exe creator                 | Yes              | No                                    |
-| Package creator                         | Yes              | Yes, but bundles aren't supported yet |
-| Bundle > compress to memory             | No               | Yes                                   |
-| Scene Hierarchy view                    | No               | Yes                                   |
-| Assets info > Dependencies              | Yes              | No                                    |
-| Assets info > Containers                | Yes              | No                                    |
-| Plugins > AudioClip                     | Yes              | No                                    |
-| Plugins > Mesh                          | Yes              | No                                    |
-| Plugins > MovieTexture                  | Yes              | No                                    |
-| Plugins > SubstanceArchive              | Yes              | No                                    |
-| Plugins > TerrainData                   | Yes              | No                                    |
-| Plugins > UMAMesh                       | Yes              | No                                    |
-| And more I probably forgot about...     |                  |                                       |
+Many games are also now using addressables. You can tell if the bundle you're opening is part of addressables because it has the path `StreamingAssets/aa/XXX/something.bundle`. You will need to clear the CRC checks with the CRC cleaning tool here.
 
 ## Libraries
 
