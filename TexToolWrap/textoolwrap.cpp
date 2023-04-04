a1253db48b6201d9c2506c51f1e8109d00b09dcf
nesquack <karsen.beck@gmail.com>
2023-04-04 00:02:21 -0500

    thanks vs for randomly reformating the entire file

--- TexToolWrap/textoolwrap.cpp
+++ TexToolWrap/textoolwrap.cpp
@@ -12,9 +12,9 @@
 #include <map>
 
 #if defined(_MSC_VER)
-#define EXPORT extern "C" __declspec(dllexport)
+	#define EXPORT extern "C" __declspec(dllexport)
 #elif defined(__GNUC__)
-#define EXPORT extern "C" __attribute__((visibility("default")))
+	#define EXPORT extern "C" __attribute__((visibility("default")))
 #endif
 
 std::map<int, void*> memoryPickup;
@@ -22,127 +22,127 @@ int nextMemoryPickupId = 0;
 
 bool GetPVRTexLibModes(int mode, PVRTuint64& pvrtlMode, PVRTexLibVariableType& pvrtlVarType) {
 	switch (mode) {
-	case 5:  pvrtlMode = PVRTGENPIXELID4('a', 'r', 'g', 'b', 8, 8, 8, 8); break; //ARGB32
-	case 14: pvrtlMode = PVRTGENPIXELID4('b', 'g', 'r', 'a', 8, 8, 8, 8); break; //BGRA32
-	case 4:  pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 'a', 8, 8, 8, 8); break; //RGBA32
-	case 3:  pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 0, 8, 8, 8, 0); break; //RGB24
-	case 2:  pvrtlMode = PVRTGENPIXELID4('a', 'r', 'g', 'b', 4, 4, 4, 4); break; //ARGB4444
-	case 13: pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 'a', 4, 4, 4, 4); break; //RGBA4444
-	case 7:  pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 0, 5, 6, 5, 0); break; //RGB565
-	case 1:  pvrtlMode = PVRTGENPIXELID4('a', 0, 0, 0, 8, 0, 0, 0); break; //Alpha8
-	case 63: pvrtlMode = PVRTGENPIXELID4('r', 0, 0, 0, 8, 0, 0, 0); break; //R8
-	case 9:  pvrtlMode = PVRTGENPIXELID4('r', 0, 0, 0, 16, 0, 0, 0); break; //R16
-	case 62: pvrtlMode = PVRTGENPIXELID4('r', 'g', 0, 0, 16, 16, 0, 0); break; //RG16
-	case 15: pvrtlMode = PVRTGENPIXELID4('r', 0, 0, 0, 16, 0, 0, 0); break; //RHalf
-	case 16: pvrtlMode = PVRTGENPIXELID4('r', 'g', 0, 0, 16, 16, 0, 0); break; //RGHalf
-	case 17: pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 'a', 16, 16, 16, 16); break; //RGBAHalf
-	case 18: pvrtlMode = PVRTGENPIXELID4('r', 0, 0, 0, 32, 0, 0, 0); break; //RFloat
-	case 19: pvrtlMode = PVRTGENPIXELID4('r', 'g', 0, 0, 32, 32, 0, 0); break; //RGFloat
-	case 20: pvrtlMode = PVRTGENPIXELID4('r', 'g', 'b', 'a', 32, 32, 32, 32); break; //RGBAFloat
-	case 41: pvrtlMode = PVRTLPF_EAC_R11; break;
-	case 42: pvrtlMode = PVRTLPF_EAC_R11; break; //idk
-	case 43: pvrtlMode = PVRTLPF_EAC_RG11; break;
-	case 44: pvrtlMode = PVRTLPF_EAC_RG11; break; //idk
-	case 34: pvrtlMode = PVRTLPF_ETC1; break;
+		case 5:  pvrtlMode = PVRTGENPIXELID4('a','r','g','b', 8, 8, 8, 8); break; //ARGB32
+		case 14: pvrtlMode = PVRTGENPIXELID4('b','g','r','a', 8, 8, 8, 8); break; //BGRA32
+		case 4:  pvrtlMode = PVRTGENPIXELID4('r','g','b','a', 8, 8, 8, 8); break; //RGBA32
+		case 3:  pvrtlMode = PVRTGENPIXELID4('r','g','b', 0 , 8, 8, 8, 0); break; //RGB24
+		case 2:  pvrtlMode = PVRTGENPIXELID4('a','r','g','b', 4, 4, 4, 4); break; //ARGB4444
+		case 13: pvrtlMode = PVRTGENPIXELID4('r','g','b','a', 4, 4, 4, 4); break; //RGBA4444
+		case 7:  pvrtlMode = PVRTGENPIXELID4('r','g','b', 0 , 5, 6, 5, 0); break; //RGB565
+		case 1:  pvrtlMode = PVRTGENPIXELID4('a', 0 , 0 , 0 , 8, 0, 0, 0); break; //Alpha8
+		case 63: pvrtlMode = PVRTGENPIXELID4('r', 0 , 0 , 0 , 8, 0, 0, 0); break; //R8
+		case 9:  pvrtlMode = PVRTGENPIXELID4('r', 0 , 0 , 0 ,16, 0, 0, 0); break; //R16
+		case 62: pvrtlMode = PVRTGENPIXELID4('r','g', 0 , 0 ,16,16, 0, 0); break; //RG16
+		case 15: pvrtlMode = PVRTGENPIXELID4('r', 0 , 0 , 0 ,16, 0, 0, 0); break; //RHalf
+		case 16: pvrtlMode = PVRTGENPIXELID4('r','g', 0 , 0 ,16,16, 0, 0); break; //RGHalf
+		case 17: pvrtlMode = PVRTGENPIXELID4('r','g','b','a',16,16,16,16); break; //RGBAHalf
+		case 18: pvrtlMode = PVRTGENPIXELID4('r', 0 , 0 , 0 ,32, 0, 0, 0); break; //RFloat
+		case 19: pvrtlMode = PVRTGENPIXELID4('r','g', 0 , 0 ,32,32, 0, 0); break; //RGFloat
+		case 20: pvrtlMode = PVRTGENPIXELID4('r','g','b','a',32,32,32,32); break; //RGBAFloat
+		case 41: pvrtlMode = PVRTLPF_EAC_R11; break;
+		case 42: pvrtlMode = PVRTLPF_EAC_R11; break; //idk
+		case 43: pvrtlMode = PVRTLPF_EAC_RG11; break;
+		case 44: pvrtlMode = PVRTLPF_EAC_RG11; break; //idk
+		case 34: pvrtlMode = PVRTLPF_ETC1; break;
 		//case 60: break; //idk
 		//case 61: break; //idk
-	case 45: pvrtlMode = PVRTLPF_ETC2_RGB; break;
-	case 46: pvrtlMode = PVRTLPF_ETC2_RGB_A1; break;
-	case 47: pvrtlMode = PVRTLPF_ETC2_RGBA; break;
-	case 30: pvrtlMode = PVRTLPF_PVRTCI_2bpp_RGB; break;
-	case 31: pvrtlMode = PVRTLPF_PVRTCI_2bpp_RGBA; break;
-	case 32: pvrtlMode = PVRTLPF_PVRTCI_4bpp_RGB; break;
-	case 33: pvrtlMode = PVRTLPF_PVRTCI_4bpp_RGBA; break;
-	case 48: pvrtlMode = PVRTLPF_ASTC_4x4; break;
-	case 49: pvrtlMode = PVRTLPF_ASTC_5x5; break;
-	case 50: pvrtlMode = PVRTLPF_ASTC_6x6; break;
-	case 51: pvrtlMode = PVRTLPF_ASTC_8x8; break;
-	case 52: pvrtlMode = PVRTLPF_ASTC_10x10; break;
-	case 53: pvrtlMode = PVRTLPF_ASTC_12x12; break;
-	case 54: pvrtlMode = PVRTLPF_ASTC_4x4; break; //idk
-	case 55: pvrtlMode = PVRTLPF_ASTC_5x5; break; //idk
-	case 56: pvrtlMode = PVRTLPF_ASTC_6x6; break; //idk
-	case 57: pvrtlMode = PVRTLPF_ASTC_8x8; break; //idk
-	case 58: pvrtlMode = PVRTLPF_ASTC_10x10; break; //idk
-	case 59: pvrtlMode = PVRTLPF_ASTC_12x12; break; //idk
-	default: return false; //idk
+		case 45: pvrtlMode = PVRTLPF_ETC2_RGB; break;
+		case 46: pvrtlMode = PVRTLPF_ETC2_RGB_A1; break;
+		case 47: pvrtlMode = PVRTLPF_ETC2_RGBA; break;
+		case 30: pvrtlMode = PVRTLPF_PVRTCI_2bpp_RGB; break;
+		case 31: pvrtlMode = PVRTLPF_PVRTCI_2bpp_RGBA; break;
+		case 32: pvrtlMode = PVRTLPF_PVRTCI_4bpp_RGB; break;
+		case 33: pvrtlMode = PVRTLPF_PVRTCI_4bpp_RGBA; break;
+		case 48: pvrtlMode = PVRTLPF_ASTC_4x4; break;
+		case 49: pvrtlMode = PVRTLPF_ASTC_5x5; break;
+		case 50: pvrtlMode = PVRTLPF_ASTC_6x6; break;
+		case 51: pvrtlMode = PVRTLPF_ASTC_8x8; break;
+		case 52: pvrtlMode = PVRTLPF_ASTC_10x10; break;
+		case 53: pvrtlMode = PVRTLPF_ASTC_12x12; break;
+		case 54: pvrtlMode = PVRTLPF_ASTC_4x4; break; //idk
+		case 55: pvrtlMode = PVRTLPF_ASTC_5x5; break; //idk
+		case 56: pvrtlMode = PVRTLPF_ASTC_6x6; break; //idk
+		case 57: pvrtlMode = PVRTLPF_ASTC_8x8; break; //idk
+		case 58: pvrtlMode = PVRTLPF_ASTC_10x10; break; //idk
+		case 59: pvrtlMode = PVRTLPF_ASTC_12x12; break; //idk
+		default: return false; //idk
 	}
-
+	
 	switch (mode) {
-	case 15: pvrtlVarType = PVRTLVT_SignedFloat; break; //RHalf
-	case 16: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGHalf
-	case 17: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGBAHalf
-	case 18: pvrtlVarType = PVRTLVT_SignedFloat; break; //RFloat
-	case 19: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGFloat
-	case 20: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGBAFloat
-	default: pvrtlVarType = PVRTLVT_UnsignedByteNorm; break;
+		case 15: pvrtlVarType = PVRTLVT_SignedFloat; break; //RHalf
+		case 16: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGHalf
+		case 17: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGBAHalf
+		case 18: pvrtlVarType = PVRTLVT_SignedFloat; break; //RFloat
+		case 19: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGFloat
+		case 20: pvrtlVarType = PVRTLVT_SignedFloat; break; //RGBAFloat
+		default: pvrtlVarType = PVRTLVT_UnsignedByteNorm; break;
 	}
-
+	
 	return true;
 }
 
 PVRTexLibCompressorQuality GetPVRTexLibCompressionLevel(PVRTuint64 pvrtlMode) {
 	switch (pvrtlMode) {
-	case PVRTLPF_PVRTCI_2bpp_RGB:
-	case PVRTLPF_PVRTCI_2bpp_RGBA:
-	case PVRTLPF_PVRTCI_4bpp_RGB:
-	case PVRTLPF_PVRTCI_4bpp_RGBA:
-	case PVRTLPF_PVRTCII_2bpp:
-	case PVRTLPF_PVRTCII_4bpp:
-		return PVRTLCQ_PVRTCNormal;
-	case PVRTLPF_ETC1:
-	case PVRTLPF_ETC2_RGB:
-	case PVRTLPF_ETC2_RGBA:
-	case PVRTLPF_ETC2_RGB_A1:
-	case PVRTLPF_EAC_R11:
-	case PVRTLPF_EAC_RG11:
-		return PVRTLCQ_ETCNormal;
-	case PVRTLPF_ASTC_4x4:
-	case PVRTLPF_ASTC_5x4:
-	case PVRTLPF_ASTC_5x5:
-	case PVRTLPF_ASTC_6x5:
-	case PVRTLPF_ASTC_6x6:
-	case PVRTLPF_ASTC_8x5:
-	case PVRTLPF_ASTC_8x6:
-	case PVRTLPF_ASTC_8x8:
-	case PVRTLPF_ASTC_10x5:
-	case PVRTLPF_ASTC_10x6:
-	case PVRTLPF_ASTC_10x8:
-	case PVRTLPF_ASTC_10x10:
-	case PVRTLPF_ASTC_12x10:
-	case PVRTLPF_ASTC_12x12:
-	case PVRTLPF_ASTC_3x3x3:
-	case PVRTLPF_ASTC_4x3x3:
-	case PVRTLPF_ASTC_4x4x3:
-	case PVRTLPF_ASTC_4x4x4:
-	case PVRTLPF_ASTC_5x4x4:
-	case PVRTLPF_ASTC_5x5x4:
-	case PVRTLPF_ASTC_5x5x5:
-	case PVRTLPF_ASTC_6x5x5:
-	case PVRTLPF_ASTC_6x6x5:
-	case PVRTLPF_ASTC_6x6x6:
-		return PVRTLCQ_ASTCMedium;
-	default:
-		return PVRTLCQ_PVRTCNormal;
+		case PVRTLPF_PVRTCI_2bpp_RGB:
+		case PVRTLPF_PVRTCI_2bpp_RGBA:
+		case PVRTLPF_PVRTCI_4bpp_RGB:
+		case PVRTLPF_PVRTCI_4bpp_RGBA:
+		case PVRTLPF_PVRTCII_2bpp:
+		case PVRTLPF_PVRTCII_4bpp:
+			return PVRTLCQ_PVRTCNormal;
+		case PVRTLPF_ETC1:
+		case PVRTLPF_ETC2_RGB:
+		case PVRTLPF_ETC2_RGBA:
+		case PVRTLPF_ETC2_RGB_A1:
+		case PVRTLPF_EAC_R11:
+		case PVRTLPF_EAC_RG11:
+			return PVRTLCQ_ETCNormal;
+		case PVRTLPF_ASTC_4x4:
+		case PVRTLPF_ASTC_5x4:
+		case PVRTLPF_ASTC_5x5:
+		case PVRTLPF_ASTC_6x5:
+		case PVRTLPF_ASTC_6x6:
+		case PVRTLPF_ASTC_8x5:
+		case PVRTLPF_ASTC_8x6:
+		case PVRTLPF_ASTC_8x8:
+		case PVRTLPF_ASTC_10x5:
+		case PVRTLPF_ASTC_10x6:
+		case PVRTLPF_ASTC_10x8:
+		case PVRTLPF_ASTC_10x10:
+		case PVRTLPF_ASTC_12x10:
+		case PVRTLPF_ASTC_12x12:
+		case PVRTLPF_ASTC_3x3x3:
+		case PVRTLPF_ASTC_4x3x3:
+		case PVRTLPF_ASTC_4x4x3:
+		case PVRTLPF_ASTC_4x4x4:
+		case PVRTLPF_ASTC_5x4x4:
+		case PVRTLPF_ASTC_5x5x4:
+		case PVRTLPF_ASTC_5x5x5:
+		case PVRTLPF_ASTC_6x5x5:
+		case PVRTLPF_ASTC_6x6x5:
+		case PVRTLPF_ASTC_6x6x6:
+			return PVRTLCQ_ASTCMedium;
+		default:
+			return PVRTLCQ_PVRTCNormal;
 	}
 }
 
 EXPORT unsigned int DecodeByPVRTexLib(void* data, void* outBuf, int mode, unsigned int width, unsigned int height) {
 	PVRTuint64 pvrtlMode;
 	PVRTexLibVariableType pvrtlVarType;
-
+	
 	if (!GetPVRTexLibModes(mode, pvrtlMode, pvrtlVarType)) {
 		return 0;
 	}
-
-	unsigned long long RGBA8888 = PVRTGENPIXELID4('r', 'g', 'b', 'a', 8, 8, 8, 8);
-	pvrtexlib::PVRTextureHeader pvrth = pvrtexlib::PVRTextureHeader(pvrtlMode, width, height, 1, 1, 1, 1, PVRTLCS_sRGB, pvrtlVarType);
+	
+	unsigned long long RGBA8888 = PVRTGENPIXELID4('r','g','b','a', 8,8,8,8);
+	pvrtexlib::PVRTextureHeader pvrth = pvrtexlib::PVRTextureHeader(pvrtlMode, width, height, 1,1,1,1, PVRTLCS_sRGB, pvrtlVarType);
 	pvrtexlib::PVRTexture pvrt = pvrtexlib::PVRTexture(pvrth, data);
-
+	
 	if (!pvrt.Transcode(RGBA8888, PVRTLVT_UnsignedByteNorm, PVRTLCS_sRGB, PVRTLCQ_PVRTCNormal, false)) {
 		return 0;
 	}
-
+	
 	void* newData = pvrt.GetTextureDataPointer();
 	unsigned int size = pvrt.GetTextureDataSize();
 	memcpy(outBuf, newData, size);
@@ -151,20 +151,20 @@ EXPORT unsigned int DecodeByPVRTexLib(void* data, void* outBuf, int mode, unsign
 EXPORT unsigned int EncodeByPVRTexLib(void* data, void* outBuf, int mode, int level, unsigned int width, unsigned int height) {
 	PVRTuint64 pvrtlMode;
 	PVRTexLibVariableType pvrtlVarType;
-
+	
 	if (!GetPVRTexLibModes(mode, pvrtlMode, pvrtlVarType)) {
 		return 0;
 	}
 	PVRTexLibCompressorQuality compLevel = GetPVRTexLibCompressionLevel(pvrtlMode);
-
-	unsigned long long RGBA8888 = PVRTGENPIXELID4('r', 'g', 'b', 'a', 8, 8, 8, 8);
+	
+	unsigned long long RGBA8888 = PVRTGENPIXELID4('r','g','b','a', 8,8,8,8);
 	pvrtexlib::PVRTextureHeader pvrth = pvrtexlib::PVRTextureHeader(RGBA8888, width, height);
 	pvrtexlib::PVRTexture pvrt = pvrtexlib::PVRTexture(pvrth, data);
-
+	
 	if (!pvrt.Transcode(pvrtlMode, pvrtlVarType, PVRTLCS_sRGB, compLevel, false)) {
 		return 0;
 	}
-
+	
 	void* newData = pvrt.GetTextureDataPointer();
 	unsigned int size = pvrt.GetTextureDataSize();
 	memcpy(outBuf, newData, size);
@@ -185,8 +185,7 @@ EXPORT unsigned int EncodeByISPC(void* data, void* outBuf, int mode, int level, 
 	if (mode == 10) { //DXT1
 		CompressBlocksBC1(&surface, (uint8_t*)outBuf);
 		blockByteSize = 8;
-	}
-	else if (mode == 12) { //DXT5
+	} else if (mode == 12) { //DXT5
 		CompressBlocksBC3(&surface, (uint8_t*)outBuf);
 		blockByteSize = 16;
 	}
@@ -197,20 +196,17 @@ EXPORT unsigned int EncodeByISPC(void* data, void* outBuf, int mode, int level, 
 	else if (mode == 27) { // BC5
 		CompressBlocksBC5(&surface, (uint8_t*)outBuf);
 		blockByteSize = 123; // todo: check
-	}
-	else if (mode == 24) { // BC6H
+	} else if (mode == 24) { // BC6H
 		bc6h_enc_settings bc6hsettings;
 		GetProfile_bc6h_basic(&bc6hsettings);
 		CompressBlocksBC6H(&surface, (uint8_t*)outBuf, &bc6hsettings);
 		blockByteSize = 123; // todo: check
-	}
-	else if (mode == 25) { //BC7
+	} else if (mode == 25) { //BC7
 		bc7_enc_settings bc7settings;
 		GetProfile_alpha_basic(&bc7settings); //GetProfile_alpha_slow
 		CompressBlocksBC7(&surface, (uint8_t*)outBuf, &bc7settings);
 		blockByteSize = 16;
-	}
-	else {
+	} else {
 		return 0;
 	}
 
@@ -242,8 +238,7 @@ EXPORT unsigned int DecodeByCrunchUnity(void* data, void* outBuf, int mode, unsi
 
 	if (success) {
 		return size_of_face;
-	}
-	else {
+	} else {
 		return 0;
 	}
 }
@@ -260,20 +255,20 @@ EXPORT unsigned int EncodeByCrunchUnity(void* data, int* checkoutId, int mode, i
 	comp_params.m_file_type = cCRNFileTypeCRN;
 
 	switch (mode) {
-	case 28:
-		comp_params.m_format = cCRNFmtDXT1;
-		break;
-	case 29:
-		comp_params.m_format = cCRNFmtDXT5;
-		break;
-	case 64:
-		comp_params.m_format = cCRNFmtETC1;
-		break;
-	case 65:
-		comp_params.m_format = cCRNFmtETC2A;
-		break;
-	default:
-		return 0;
+		case 28:
+			comp_params.m_format = cCRNFmtDXT1;
+			break;
+		case 29:
+			comp_params.m_format = cCRNFmtDXT5;
+			break;
+		case 64:
+			comp_params.m_format = cCRNFmtETC1;
+			break;
+		case 65:
+			comp_params.m_format = cCRNFmtETC2A;
+			break;
+		default:
+			return 0;
 	}
 
 	comp_params.m_pImages[0][0] = (crn_uint32*)data;
@@ -290,14 +285,12 @@ EXPORT unsigned int EncodeByCrunchUnity(void* data, int* checkoutId, int mode, i
 	// hope that doesn't happen here :shrugs:
 	if (mips > cCRNMaxLevels) {
 		mips = cCRNMaxLevels;
-	}
-	else if (mips < 0) {
+	} else if (mips < 0) {
 		mips = 1;
 	}
 	if (mips == 1) {
 		mip_params.m_mode = cCRNMipModeNoMips;
-	}
-	else {
+	} else {
 		mip_params.m_mode = cCRNMipModeGenerateMips;
 	}
 
@@ -316,15 +309,14 @@ EXPORT unsigned int EncodeByCrunchUnity(void* data, int* checkoutId, int mode, i
 		}
 
 		memcpy(outBuf, newData, output_file_size);
-
+		
 		// todo: not thread safe (although we don't do any threading right now)
 		*checkoutId = nextMemoryPickupId;
 		memoryPickup[nextMemoryPickupId] = outBuf;
 		nextMemoryPickupId++;
 
 		return output_file_size;
-	}
-	else {
+	} else {
 		return 0;
 	}
 }
